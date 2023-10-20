b14b2670133fa79f1ddcc0689f3f7e3f8c6c00a2
C-256 <hydrogen1.008@petalmail.com>
2023-10-20 17:09:40 +0800

    Change into CMake project

---
+++ BT.h
@@ -0,0 +1,72 @@
+﻿#pragma once
+
+#include <iostream>
+
+struct N
+{
+	char value;
+	N* l;
+	N* r;
+};
+
+template <class T>
+struct L
+{
+	T value;
+	L<T>* next;
+};
+
+template <class T>
+struct Stack
+{
+	L<T>* top;
+
+	Stack();
+
+	void Push(T value);
+	void Push(T* value);
+	void Pop();
+	bool IsEmpty();
+	void Clear();
+	void Print();
+};
+
+template <class T>
+struct Queue
+{
+	L<T>* front;
+	L<T>* rear;
+
+	Queue();
+
+	void Push(T value);
+	void Push(T* value);
+	void Pop();
+	bool IsEmpty();
+	void Clear();
+	void Print();
+};
+
+struct BT
+{
+	N* root;
+
+	BT();
+	BT(N* sub);
+	BT(char value);
+
+	static N* Initialise(int position);
+	void PreorderTraverse();
+	void InorderTraverse();
+	void PostorderTraverse();
+	void LevelOrderTraverse();
+	void RPreorderTraverse();
+	void RInorderTraverse();
+	void RPostorderTraverse();
+	int Height();
+	int Width();
+	bool IsComplete();
+	void HeightDetector(int position, int worstLen, int* positionArr);
+	void HeightDetector(int position, int& deepest);
+	void WidthDetector(int position, int height, int* temp);
+};
--- BinaryTree.cpp
+++ BinaryTree.cpp
@@ -1,75 +1,6 @@
-﻿#include <iostream>
+﻿#include "BinaryTree.h"
 
 using namespace std;
-
-struct N
-{
-	char value;
-	N* l;
-	N* r;
-};
-
-template <class T>
-struct L
-{
-	T value;
-	L<T>* next;
-};
-
-template <class T>
-struct Stack
-{
-	L<T>* top;
-
-	Stack();
-
-	void Push(T value);
-	void Push(T* value);
-	void Pop();
-	bool IsEmpty();
-	void Clear();
-	void Print();
-};
-
-template <class T>
-struct Queue
-{
-	L<T>* front;
-	L<T>* rear;
-
-	Queue();
-
-	void Push(T value);
-	void Push(T* value);
-	void Pop();
-	bool IsEmpty();
-	void Clear();
-	void Print();
-};
-
-struct BT
-{
-	N* root;
-
-	BT();
-	BT(N* sub);
-	BT(char value);
-
-	static N* Initialise(int position);
-	void PreorderTraverse();
-	void InorderTraverse();
-	void PostorderTraverse();
-	void LevelOrderTraverse();
-	void RPreorderTraverse();
-	void RInorderTraverse();
-	void RPostorderTraverse();
-	int Height();
-	int Width();
-	bool IsComplete();
-	void HeightDetector(int position, int worstLen, int* positionArr);
-	void HeightDetector(int position, int& deepest);
-	void WidthDetector(int position, int height, int* temp);
-};
 
 int main(int argc, char** argv)
 {
@@ -526,7 +457,7 @@ int BT::Width()
 	}
 
 	WidthDetector(1, h, arr);
-	
+
 	for (int i = 0; i < h; i++)
 	{
 		res = res > arr[i] ? res : arr[i];
